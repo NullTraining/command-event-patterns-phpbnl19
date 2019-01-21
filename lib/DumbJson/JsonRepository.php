c9e5fc69ab4383d56becfd554780b3fbfd7efdaf
Miro Svrtan <miro@mirosvrtan.me>
2019-01-21 15:56:30 +0100

    Add DumpJsonRepository

---
+++ lib/DumbJson/JsonRepository.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DumbJson;
+
+abstract class JsonRepository
+{
+    public function __construct()
+    {
+        $this->env = getenv('APP_ENV');
+    }
+
+    public function truncate()
+    {
+        $this->setTableContent([]);
+    }
+
+    abstract protected function getTableName(): string;
+
+    protected function add($entity): void
+    {
+        $content = $this->getTableContent();
+
+        $content[$entity->getEntityId()] = $entity->serialize();
+
+        $this->setTableContent($content);
+    }
+
+    protected function find(string $id)
+    {
+        $content = $this->getTableContent();
+
+        if (array_key_exists($id, $content)) {
+            return $this->deserialize($content[$id]);
+        }
+
+        return null;
+    }
+
+    private function setTableContent(array $content): void
+    {
+        $filename = $this->getFileName();
+
+        $json = json_encode($content, JSON_PRETTY_PRINT);
+
+        file_put_contents($filename, $json);
+    }
+
+    private function getTableContent(): array
+    {
+        $filename = $this->getFileName();
+
+        if (false === is_file($filename)) {
+            $this->setTableContent([]);
+
+            return [];
+        }
+
+        $json = file_get_contents($filename);
+
+        return json_decode($json, true);
+    }
+
+    private function deserialize(array $item)
+    {
+        return $item['__className']::deserialize($item);
+    }
+
+    private function getFileName(): string
+    {
+        return __DIR__.'/../../data/'.$this->env.'/'.$this->getTableName().'.json';
+    }
+
+    protected function getResults(): array
+    {
+        $content = $this->getTableContent();
+
+        $result = [];
+
+        foreach ($content as $item) {
+            $result[] = $this->deserialize($item);
+        }
+
+        return $result;
+    }
+}
