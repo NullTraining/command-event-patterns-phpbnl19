836621fe68bf3a0d69c46625dbc1e8e023915d57
Miro Svrtan <miro@mirosvrtan.me>
2019-01-21 15:56:47 +0100

    Add PhoneNumber VO

---
+++ src/Generic/PhoneNumber.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Generic;
+
+class PhoneNumber
+{
+    /** @var string */
+    private $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+}
