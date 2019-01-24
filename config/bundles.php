cf0808e5335f8f1aaf50af4cc0bdaf2e1e096177
Miro Svrtan <miro@mirosvrtan.me>
2019-01-24 07:55:02 +0100

    Include SimpleBus bundles

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle::class => ['all' => true],
+    SimpleBus\SymfonyBridge\SimpleBusEventBusBundle::class => ['all' => true],
+
 ];
